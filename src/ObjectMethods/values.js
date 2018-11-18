0ea544697f33f0d3d43d0f123f670bb5571cbad4
Alex Lesnicky <alex.lesnicky@gmail.com>
2018-11-18 04:54:30 +0300

    add values method

---
+++ src/ObjectMethods/values.js
@@ -0,0 +1,11 @@
+const values = (obj) => {
+    const arrResValues = [];
+    for (let key in obj) {
+        if (obj.hasOwnProperty(key)) {
+            arrResValues.push(obj[key]);
+        }
+    }
+    return arrResValues;
+}
+
+module.exports = values;
