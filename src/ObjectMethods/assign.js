e4790bcf249885ab17c7e2172d00c647de7777f9
Alex Lesnicky <alex.lesnicky@gmail.com>
2018-11-18 04:36:51 +0300

    fix assign method

--- src/ObjectMethods/assign.js
+++ src/ObjectMethods/assign.js
@@ -1,14 +1,13 @@
 const keys = require('./keys');
 
 const assign = (obj, ...arg) => {
-    const objResAssign = JSON.parse(JSON.stringify(obj));
     for (let i = 0; i < arg.length; i++) {
         const masProp = keys(arg[i]);
         for (let j = 0; j < masProp.length; j++) {
-            objResAssign[masProp[j]] = arg[i][masProp[j]];
+            obj[masProp[j]] = arg[i][masProp[j]];
         }
     }
-    return objResAssign;
+    return obj;
 }
 
 module.exports = assign;
